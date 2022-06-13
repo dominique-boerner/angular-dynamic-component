a154692f9d02ffb18e4ef5b1056e015db6aba6ec
mukosoft <dominique@mukosoft.de>
2022-06-13 18:56:27 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,9 @@ import { CardComponent } from './components/card/card.component';
 import { CardBodyComponent } from './components/card/templates/card-body/card-body.component';
 import { CardHeadComponent } from './components/card/templates/card-head/card-head.component';
 import { TagComponent } from './components/tag/tag.component';
-import { AppComponent as DynamicAppModule } from 'ng-dynamic-components/src/app/app.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, DynamicAppModule],
+  imports: [BrowserModule, FormsModule],
   declarations: [
     AppComponent,
     CardComponent,
